738290d5bc408d4f82f07c0708bf6f23423047de
Althaf <abdulla@innoventes.co>
2026-01-10 12:42:05 +0530

    create express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+
+
+
+
+app.listen(PORT,()=>{
+  console.log("server is running",PORT)
+})
+
